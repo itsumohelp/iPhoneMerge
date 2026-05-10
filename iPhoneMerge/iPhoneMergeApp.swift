3866ad7cfe0ed9edd4b8a7d457fda1ffa2bf471a
KaiKusakari <kai.kusakari@gmail.com>
2026-05-10 09:40:01 +0900

    Initial Commit

---
+++ iPhoneMerge/iPhoneMergeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneMergeApp.swift
+//  iPhoneMerge
+//
+//  Created by kusakari on 2026/05/10.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneMergeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
